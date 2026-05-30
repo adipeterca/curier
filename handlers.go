12adb0b509a96219cb807b1c6f6747f0e7508456
Adrian PETERCA <adipeterca@gmail.com>
2026-05-30 13:22:43 +0300

    Refactored embedded file storage + README update

--- handlers.go
+++ handlers.go
@@ -14,7 +14,14 @@ import (
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/index.html")
+	data, err := staticFiles.ReadFile("static/index.html")
+	if err != nil {
+		fmt.Printf("ERROR: static/index.html not found in embedded files")
+		http.Error(w, "Sorry :(", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(data)
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -145,7 +152,8 @@ func shareHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "text/html")
-	tmpl, err := template.ParseFiles("templates/share.html")
+	tmpl, err := template.ParseFS(templateFiles, "templates/share.html")
+
 	if err != nil {
 		fmt.Printf("ERROR: could not parse template: %s\n", err)
 		http.Error(w, "Sorry, I did my best :(", http.StatusInternalServerError)
@@ -160,7 +168,14 @@ func shareHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func cssHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/styles.css")
+	data, err := staticFiles.ReadFile("static/styles.css")
+	if err != nil {
+		fmt.Printf("ERROR: static/styles.css not found in embedded files")
+		http.Error(w, "Sorry :(", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/css; charset=utf-8")
+	w.Write(data)
 }
 
 // --- Helper functions ---
